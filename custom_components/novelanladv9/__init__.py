82b8d1f351cbab99754568f301d30f1ad470f1f4
LordZeroMS <github_code@lordzero.de>
2025-09-17 16:20:10 +0200

    Log integration setup

--- custom_components/novelanladv9/__init__.py
+++ custom_components/novelanladv9/__init__.py
@@ -1,6 +1,12 @@
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+
+LOGGER = logging.getLogger(__name__)
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    LOGGER.info("Setting up Novelan LADV9 integration")
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "select", "number"])
     return True
